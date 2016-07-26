92ee7038775c8f63cc0e61da5ce5334ae6f0d42e
zhutao <zhutao@stu.ecnu.edu.cn>
2016-07-26 15:51:56 +0800

    small fix

--- src/sql/ob_sql_session_info.cpp
+++ src/sql/ob_sql_session_info.cpp
@@ -136,7 +136,6 @@ void ObSQLSessionInfo::destroy()
     }
     else
     {
-      TBSYS_LOG(INFO, "destory stmtid: %ld", iter->first);
       result_set_pool_.free(result_set);
       id_plan_map_.erase(iter->first);
     }
